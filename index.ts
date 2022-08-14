5e69b523df7e13a64faf0f9939254a1ed79beab9
zara <zara12138@gmail.com>
2022-08-14 22:14:35 +1000

    task 5

--- index.ts
+++ index.ts
@@ -1,8 +1,24 @@
 import { task1 } from './task1';
+import { task3 } from './task3';
+import { task4 } from './task4';
+import { task5 } from './task5';
 
 const hello = async (): Promise<void> => {
   console.log('Hello World');
-  await task1();
+
+  console.log(`----------------------This is task 1-----------------------`);
+  const result1 = await task1();
+  console.log(result1)
+
+  console.log(`----------------------This is task 3-----------------------`);
+  await task3();
+
+  console.log(`----------------------This is task 4-----------------------`);
+  const result4 = await task4();
+  console.log(result4);
+
+  console.log(`----------------------This is task 5-----------------------`);
+  await task5();
 
 }
 hello();
--- task5.ts
+++ task5.ts
@@ -1,7 +1,32 @@
 import * as dotenv from 'dotenv'
+import AWS from 'aws-sdk';
+import { PutObjectRequest } from 'aws-sdk/clients/s3';
+import { task1 } from './task1';
 
 export const task5 = async (): Promise<void> => {
   dotenv.config();
   console.log(`AWS_ACCESS_KEY_ID=${process.env.AWS_ACCESS_KEY_ID}`);
   console.log(`AWS_SECRET_ACCESS_KEY=${process.env.AWS_SECRET_ACCESS_KEY}`);
+
+  task1().then(data => {
+    let csv = data.join('\r\n');
+
+    const s3 = new AWS.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    });
+    const params: PutObjectRequest = {
+      Bucket: 'inquisitive-backend-developer-tests',
+      Key: 'zara/year_subject_lesson.csv',
+      Body: csv
+    };
+    s3.putObject(params, (error: any, data: any) => {
+      if (error) {
+        console.error(error, error.stack);
+        return;
+      }
+      console.log(data);
+      console.log('File uploaded successfully');
+    })
+  })
 }
